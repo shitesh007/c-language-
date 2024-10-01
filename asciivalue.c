2711bcb0ca671dcc1a2906a1be249d6e15f0a54d
shitesh kumar <sitesh9334@gmail.com>
2024-10-01 09:26:28 +0530

    Add files via upload

---
+++ asciivalue.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    char ch;
+    printf("enter the ch :");
+    scanf("%c",&ch);
+    printf("the ASCII value of %c is %d\n",ch,ch);
+    int a;
+    printf("enter a int :");
+    scanf("%d",&a);
+    printf("the reverse ASCII value of this int is %d is %c\n",a,a);
+
+    return 0;
+}
